d67287ecc3f6f8c0403a36881272b0dcd4df52e2
Piotr Wald <piotr.wald@chimeraprime.com>
2019-03-03 11:01:58 +0100

    Rebuild the gem

--- Gemfile
+++
@@ -1,3 +0,0 @@
-source 'https://rubygems.org' do
-  gem 'dry-struct', '~> 0.6.0'
-end
--- lib/halumi.rb
+++ lib/halumi.rb
@@ -1,6 +1,1 @@
-require 'active_support/core_ext/class/attribute.rb'
-require 'active_record'
-require 'dry-struct'
-
-# Load all ruby source files
-Dir['./**/*.rb'].each { |f| require f }
+require 'halumi/query'
